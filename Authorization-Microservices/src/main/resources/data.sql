9d977eb84ad7286b91666c4c645b88a69c9d6455
saite <saite@SAITEJA>
2022-03-14 11:15:31 +0530

    commit

---
+++ Authorization-Microservices/src/main/resources/data.sql
@@ -0,0 +1,6 @@
+insert into users values(1,'123456789','Saiteja');
+insert into users values(2,'123456789','Pranay');
+insert into users values(3,'123456789','Lohith');
+insert into users values(4,'123456789','Reena');
+insert into users values(5,'123456789','Chandu');
+insert into users values(6,'123456789','Ajay');
